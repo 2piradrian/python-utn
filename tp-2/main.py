caec6fc37edc6811d1545d637d6f9e9c887e51ee
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-06-15 16:00:46 -0300

    reading a file

--- tp-2/main.py
+++ tp-2/main.py
@@ -1,6 +1,41 @@
-# aca vamos a hacer el tp 2
-#hola soy juan
+# lee el archivo de patentes
+def readPlateList():
+    return open("peaje100.txt", "rt")
 
-#hola soy jeipi
-#merge
-#patata frita
+
+# lee patentes linea por linea
+def readPlate(plateList):
+    isFirstLine = True
+
+    while True:
+        line = plateList.readline()
+        # < ---- Checking lines ---- > #
+        if isFirstLine:
+            isFirstLine = False
+            continue
+
+        if line == "":
+            break
+
+        if line[:1] == " ":
+            line = line[1:]
+        # < ----!- --- -- --- -!---- > #
+
+        print(line)
+        
+
+# detecta el pais de origen
+def detectCountry(plate):
+    if (len(plate) < 6 or len(plate) > 7):
+        return "Error"
+    
+
+def main():
+    plateList = readPlateList()
+    
+    readPlate(plateList)
+
+main()
+
+
+
